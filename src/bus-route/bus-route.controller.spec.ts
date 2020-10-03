d19c869a3b1174dbf508b31eba4ea790aa1227ef
Pin Lin <moneycat711@gmail.com>
2020-10-04 02:36:12 +0800

    Fix tests of BusRouteController

--- src/bus-route/bus-route.controller.spec.ts
+++ src/bus-route/bus-route.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PtxModule } from '../ptx/ptx.module';
 import { BusRouteController } from './bus-route.controller';
+import { BusRouteService } from './bus-route.service';
 
 describe('BusRoute Controller', () => {
   let controller: BusRouteController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PtxModule],
+      providers: [BusRouteService],
       controllers: [BusRouteController],
     }).compile();
 
@@ -14,5 +18,30 @@ describe('BusRoute Controller', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should getBusRoutes correctly', async () => {
+    const result = await controller.getBusRoutes('Keelung');
+    expect(result).toBeDefined();
+    expect(result.length).toBeGreaterThan(0);
+
+    const busRoute = result[0];
+    expect(busRoute.id).toBeDefined();
+    expect(busRoute.nameZhTw).toBeDefined();
+    expect(busRoute.nameEn).toBeDefined();
+    expect(busRoute.departureStopNameZhTw).toBeDefined();
+    expect(busRoute.departureStopNameEn).toBeDefined();
+    expect(busRoute.destinationStopNameZhTw).toBeDefined();
+    expect(busRoute.destinationStopNameEn).toBeDefined();
+    expect(busRoute.city).toBeDefined();
+    expect(busRoute.versionId).toBeDefined();
+    expect(busRoute.subRoutes).toBeDefined();
+    expect(busRoute.subRoutes.length).toBeGreaterThan(0);
+
+    const subRoute = busRoute.subRoutes[0];
+    expect(subRoute.id).toBeDefined();
+    expect(subRoute.direction).toBeDefined();
+    expect(subRoute.nameZhTw).toBeDefined();
+    expect(subRoute.nameEn).toBeDefined();
   });
 });
