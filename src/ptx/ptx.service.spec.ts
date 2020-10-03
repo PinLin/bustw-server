a8cb71e9a6458d6b647edf2e338b03d5a8b182a5
Pin Lin <moneycat711@gmail.com>
2020-10-04 02:19:11 +0800

    Fix tests of PtxService

--- src/ptx/ptx.service.spec.ts
+++ src/ptx/ptx.service.spec.ts
@@ -1,16 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PtxService } from './ptx.service';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/common';
 
 describe('PtxService', () => {
   let service: PtxService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        PtxService,
-        ConfigService,
+      imports: [
+        ConfigModule.forRoot(),
+        HttpModule,
       ],
+      providers: [PtxService],
     }).compile();
 
     service = module.get<PtxService>(PtxService);
@@ -18,5 +20,36 @@ describe('PtxService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should fetchPtxDataVersion correctly', async () => {
+    const result = await service.fetchPtxDataVersion('Keelung');
+    expect(result).toBeDefined();
+    expect(result.VersionID).toBeDefined();
+    expect(result.UpdateTime).toBeDefined();
+  });
+
+  it('should fetchPtxBusRoutes correctly', async () => {
+    const result = await service.fetchPtxBusRoutes('Keelung');
+    expect(result).toBeDefined();
+    expect(result.length).toBeGreaterThan(0);
+
+    const busRoute = result[0];
+    expect(busRoute.RouteUID).toBeDefined();
+    expect(busRoute.RouteName).toBeDefined();
+    expect(busRoute.SubRoutes).toBeDefined();
+    expect(busRoute.SubRoutes.length).toBeGreaterThan(0);
+
+    const subBusRoute = busRoute.SubRoutes[0];
+    expect(subBusRoute.SubRouteUID).toBeDefined();
+    expect(subBusRoute.SubRouteName).toBeDefined();
+    expect(subBusRoute.Direction).toBeDefined();
+
+    expect(busRoute.DepartureStopNameZh).toBeDefined();
+    expect(busRoute.DepartureStopNameEn).toBeDefined();
+    expect(busRoute.DestinationStopNameZh).toBeDefined();
+    expect(busRoute.DestinationStopNameEn).toBeDefined();
+    expect(busRoute.City).toBeDefined();
+    expect(busRoute.VersionID).toBeDefined();
   });
 });
