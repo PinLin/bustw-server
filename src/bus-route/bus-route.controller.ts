aa59270d59cebffe2cc532cba94773f223863087
Pin Lin <pinlin945@gmail.com>
2021-01-25 02:27:38 +0800

    Use cache in BusRouteController

--- src/bus-route/bus-route.controller.ts
+++ src/bus-route/bus-route.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param, ForbiddenException, BadGatewayException } from '@nestjs/common';
+import { Controller, Get, Param, ForbiddenException, BadGatewayException, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { BusRouteService } from './bus-route.service';
+import { BusRoute } from './model/bus-route.model';
 
 @Controller('routes')
 export class BusRouteController {
   constructor(
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
     private readonly busRouteService: BusRouteService,
   ) { }
 
@@ -13,10 +16,25 @@ export class BusRouteController {
       throw new ForbiddenException();
     }
 
+    // 確認路線資訊是否存在於快取中
+    const cacheBusRoutes = JSON.parse(await this.cache.get(`${city}/BusRoutes`) ?? null) as BusRoute[];
+    if (cacheBusRoutes) {
+      return {
+        routes: cacheBusRoutes,
+      };
+    }
+
+    // 將路線資訊儲存到快取中
     const busRoutes = await this.busRouteService.getBusRoutes(city);
     if (!busRoutes) {
       throw new BadGatewayException();
     }
+
+    // 將路線資訊儲存到快取中
+    this.cache.set(`${city}/BusRoutes`, JSON.stringify(busRoutes), {
+      ttl: 3600,
+    });
+
     return {
       routes: busRoutes,
     };
--- src/bus-route/bus-route.module.ts
+++ src/bus-route/bus-route.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { BusRouteService } from './bus-route.service';
 import { BusRouteController } from './bus-route.controller';
 import { PtxModule } from '../ptx/ptx.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PtxModule],
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        store: redisStore,
+        host: config.get('REDIS_HOST') ?? 'localhost',
+        port: config.get('REDIS_PORT') ?? 6379,
+      }),
+      inject: [ConfigService],
+    }),
+    PtxModule,
+  ],
   providers: [BusRouteService],
   controllers: [BusRouteController]
 })
