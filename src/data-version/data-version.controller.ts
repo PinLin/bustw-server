152e9c3875e25909cf00db856ab0bbb9b4596d3d
Pin Lin <pinlin945@gmail.com>
2021-01-25 02:05:31 +0800

    Use cache in DataVersionController

--- src/data-version/data-version.controller.ts
+++ src/data-version/data-version.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param, ForbiddenException, BadGatewayException } from '@nestjs/common';
+import { Controller, Get, Param, ForbiddenException, BadGatewayException, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { DataVersionService } from './data-version.service';
+import { DataVersion } from './model/data-version.model';
 
 @Controller('data-version')
 export class DataVersionController {
   constructor(
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
     private readonly dataVersionService: DataVersionService,
   ) { }
 
@@ -13,10 +16,26 @@ export class DataVersionController {
       throw new ForbiddenException();
     }
 
+    // 確認版本資訊是否存在於快取中
+    const cacheDataVersion = JSON.parse(await this.cache.get(`${city}/DataVersion`) ?? null) as DataVersion;
+    if (cacheDataVersion) {
+      return cacheDataVersion;
+    } else {
+      // TODO: 在這裡刪除 BusRoutes 的快取是下下策
+      await this.cache.del(`${city}/BusRoutes`);
+    }
+
+    // 取得版本資訊
     const dataVersion = await this.dataVersionService.getDataVersion(city);
     if (!dataVersion) {
       throw new BadGatewayException();
     }
+
+    // 將版本資訊儲存到快取中
+    this.cache.set(`${city}/DataVersion`, JSON.stringify(dataVersion), {
+      ttl: 3600,
+    });
+
     return dataVersion;
   }
 }
--- src/data-version/data-version.module.ts
+++ src/data-version/data-version.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { DataVersionService } from './data-version.service';
 import { DataVersionController } from './data-version.controller';
 import { PtxModule } from '../ptx/ptx.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PtxModule],
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        store: redisStore,
+        host: config.get('REDIS_HOST') ?? 'localhost',
+        port: config.get('REDIS_PORT') ?? 6379,
+      }),
+      inject: [ConfigService],
+    }),
+    PtxModule,
+  ],
   providers: [DataVersionService],
   controllers: [DataVersionController],
 })
