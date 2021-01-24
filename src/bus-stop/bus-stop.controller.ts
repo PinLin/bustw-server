b2c2fa1f304a9834e155315aa85ea4d39813b54f
Pin Lin <pinlin945@gmail.com>
2021-01-25 02:41:44 +0800

    Use cache in BusStopController

--- src/bus-stop/bus-stop.controller.ts
+++ src/bus-stop/bus-stop.controller.ts
@@ -1,9 +1,12 @@
-import { BadGatewayException, Controller, ForbiddenException, Get, Param } from '@nestjs/common';
+import { BadGatewayException, CACHE_MANAGER, Controller, ForbiddenException, Get, Inject, Param } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { BusStopService } from './bus-stop.service';
+import { BusStop } from './model/bus-stop.model';
 
 @Controller('stops')
 export class BusStopController {
   constructor(
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
     private readonly busStopService: BusStopService,
   ) { }
 
@@ -13,10 +16,25 @@ export class BusStopController {
       throw new ForbiddenException();
     }
 
+    // 確認站牌資訊是否存在於快取中
+    const cacheBusStops = JSON.parse(await this.cache.get(`${city}/BusStops`) ?? null) as BusStop[];
+    if (cacheBusStops) {
+      return {
+        stops: cacheBusStops,
+      };
+    }
+
+    // 將站牌資訊儲存到快取中
     const busStops = await this.busStopService.getBusStopsByRoute(city, routeId);
     if (!busStops) {
       throw new BadGatewayException();
     }
+
+    // 將站牌資訊儲存到快取中
+    this.cache.set(`${city}/BusStops`, JSON.stringify(busStops), {
+      ttl: 10,
+    });
+
     return {
       stops: busStops,
     };
